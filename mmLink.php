4cdc03ddd573f0a716eeed97d7c90bc0a63590c7
alezakos <electrovesta@gmail.com>
2020-03-01 23:51:10 +0200

    Add legacy IRC colours that are converted to markdown

--- mmLink.php
+++ mmLink.php
@@ -1,5 +1,54 @@
 <?php
 
+<?php
+
+abstract class Colour
+{
+    const White = 0;
+    const Black = 1;
+    const Blue = 2;
+    const Green = 3;
+    const Red = 4;
+    const Brown = 5;
+    const Purple = 6;
+    const Orange = 7;
+    const Yellow = 8;
+    const LimeGreen = 9;
+    const Turquise = 10;
+    const Cyan = 11;
+    const LightBlue = 12;
+    const Pink = 13;
+    const Grey = 14;
+    const LightGrey = 15;
+    const Transparent = -1;
+}
+
+abstract class ControlCode {
+    const Bold            = 0x02;     /**< Bold */
+    const Color           = 0x03;     /**< Color */
+    const Italic          = 0x09;     /**< Italic */
+    const StrikeThrough           = 0x13;     /**< Strike-Through */
+    const Reset           = 0x0f;     /**< Reset */
+    const Underline2       = 0x15;     /**< Underline */
+    const Underline      = 0x1f;     /**< Underline */
+    //const Underline      = 037;     /**< Underline */
+    const Reverse         = 0x16;     /**< Reverse */
+};
+
+function ircColour($string, $foreground, $background=NULL) {
+    return "**$string**";
+}
+
+function ircControl($string, $control) {
+    if ($control == ControlCode::Bold) {
+        return "**$string**";
+    } else if (substr($control,-1) == ControlCode::Color) {
+        return "*$string*";
+    } else {
+        return "`$string`";
+    }
+}
+
 function sendMmMessage($message, $hook = 'njdafsn32aoalf45014klr') {
   $fields_string="";
 
